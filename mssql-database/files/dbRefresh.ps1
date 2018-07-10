a7c54128bb12ac6bda576906304fbc7f9ecd1626
Ron Ekins <ron.ekins@gmail.com>
2018-07-10 10:34:10 +0100

    MSSQL database updates

--- mssql-database/files/dbRefresh.ps1
+++
@@ -1,5 +0,0 @@
-#Import-Module PureStorageDbaTools
-
-#Get-DbaDatabase -sqlinstance z-stn-win2016-a\devopsprd -Database tpch-no-compression
-
-Invoke-Command -ComputerName z-stn-win2016-a -ScriptBlock { Write-Host "Test" }
